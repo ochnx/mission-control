2f62675d131ce6cad01eb51eba45348fe3c20e07
Oskar Neftel <oskarneftel@mac-mini.speedport.ip>
2026-02-13 10:01:44 +0100

    Static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/mission-control',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
